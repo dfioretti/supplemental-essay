beef996760b265ff651ddf5da410e46bc3c3208f
David Fioretti <fioretti.david@gmail.com>
2013-11-29 13:20:49 -0500

    initial commit

---
+++ supplemental-essay/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SupplementalEssay::Application.config.secret_key_base = 'c707d428e8ad88f9c42c51dc477679bdfbd49984ac4b1a8c5409e3bc0ebb91176a88c6f026dc6d46a38ac393fe1b3e2a8bb899f64fe9f5cdd0a01c0c06fca7e9'
